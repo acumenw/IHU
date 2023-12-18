086435468a1b030d49facd3b85c18de614e2f9f8
CStilidis <cstilidis@gmail.com>
2023-12-18 02:30:06 +0200

    update

--- Master/Web Programming/Frond End/Coursework/Project/script.js
+++ Master/Web Programming/Frond End/Coursework/Project/script.js
@@ -23,11 +23,40 @@ function getAuthor() {
           .getElementById("searchInfo")
           .setAttribute("class", "successMessages");
         document.getElementById("searchInfo").textContent = "Found";
+        var table = document.createElement("table");
+        document.body.append(table);
+        table.setAttribute("id", "pubs");
+
+        // var title = authorList.result.hits.hit[0].info["title"];
+        // var venue = authorList.result.hits.hit[0].info["venue"];
+        // var year = authorList.result.hits.hit[0].info["year"];
+        // var type = authorList.result.hits.hit[0].info["type"];
+        // document.getElementById("pubs").innerHTML +=
+        //   "<tr><td>" +
+        //   title +
+        //   "</td><td>" +
+        //   venue +
+        //   "</td><td>" +
+        //   year +
+        //   "</td><td>" +
+        //   type +
+        //   "</td></tr>";
+
         for (var i = 0; i < totalHits; i++) {
-          console.log(authorList.result.hits.hit[i].info["title"]);
-          console.log(authorList.result.hits.hit[i].info["venue"]);
-          console.log(authorList.result.hits.hit[i].info["year"]);
-          console.log(authorList.result.hits.hit[i].info["type"]);
+          var title = authorList.result.hits.hit[i].info["title"];
+          var venue = authorList.result.hits.hit[i].info["venue"];
+          var year = authorList.result.hits.hit[i].info["year"];
+          var type = authorList.result.hits.hit[i].info["type"];
+          document.getElementById("pubs").innerHTML +=
+            "<tr><td>" +
+            title +
+            "</td><td>" +
+            venue +
+            "</td><td>" +
+            year +
+            "</td><td>" +
+            type +
+            "</td></tr>";
         }
       }
     })
