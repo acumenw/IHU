21d8b0dc9af9d1468cb9c47bc5b8b9c099e18025
CStilidis <cstilidis@gmail.com>
2023-12-26 21:04:35 +0200

    change path

--- Master/Web Programming/Back End/2.12.23/upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-$temp = $_FILES["file_selector"]["tmp_name"];
-$perm = "uploads/" . $_FILES["file_selector"]["name"];
-if (move_uploaded_file($temp, $perm)) {
-	echo "The file was uploaded and moved succssfully.";
-} else {
-	echo "There was a problem moving the file";
-}
-?>
